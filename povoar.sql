a02fb228fdda46e4494f9d18d8fe9904ade0a904
Pedro Figueiredo <up201904675@fe.up.pt>
2021-03-31 19:33:32 +0100

    Started povoar.sql

---
+++ povoar.sql
@@ -0,0 +1,120 @@
+INSERT INTO Client (
+    NIF,
+    ClientName,
+    BirthDate,
+    CPoints
+)
+VALUES (
+    280727909,
+    "Maria Miguel Diogo Figueiredo",
+    02-04-2001,
+    '25'
+);
+
+INSERT INTO Client (
+    NIF,
+    ClientName,
+    BirthDate,
+    CPoints
+)
+VALUES (
+    250323906,
+    "Ana Rita Antunes Ramada",
+    18-10-2001,
+    '50'
+);
+
+INSERT INTO Client (
+    NIF,
+    ClientName,
+    BirthDate,
+    CPoints
+)
+VALUES (
+    237439581,
+    "Joaquim da Costa Silva",
+    15-09-1998,
+    '0'
+);
+
+INSERT INTO Client (
+    NIF,
+    ClientName,
+    BirthDate,
+    CPoints
+)
+VALUES (
+    248420593,
+    "Inês Maria Costa",
+    5-12-1980,
+    '0'
+);
+
+INSERT INTO Client (
+    NIF,
+    ClientName,
+    BirthDate,
+    CPoints
+)
+VALUES (
+    266423604,
+    "Margarida Fernandes Coelho",
+    27-07-2000,
+    '25'
+);
+
+
+INSERT INTO Ticket(
+    Type,
+    TPrice,
+    TPoints
+)
+VALUES (
+    "Bebé",
+    0.0,
+    0
+);
+
+INSERT INTO Ticket(
+    Type,
+    TPrice,
+    TPoints
+)
+VALUES (
+    "Criança",
+    14.50,
+    15
+);
+
+INSERT INTO Ticket(
+    Type,
+    TPrice,
+    TPoints
+)
+VALUES (
+    "Adulto",
+    22.50,
+    25
+);
+
+INSERT INTO Ticket(
+    Type,
+    TPrice,
+    TPoints
+)
+VALUES (
+    "Sénior",
+    16.0,
+    15
+);
+
+INSERT INTO Ticket(
+    Type,
+    TPrice,
+    TPoints
+)
+VALUES (
+    "Grupo",
+    18.50,
+    20
+);
